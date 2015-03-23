0e1c118691411daedef96e2eee4978978f4a9194
Jason <devilshaircut@gmail.com>
2015-03-23 15:41:04 -0400

    A functional sketch of the tile matching game.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
   var numberOfRows = $("ol#tiles li ol").length;
@@ -39,7 +40,8 @@ $(document).ready(function() {
     for(var i = 1; i <= gridSize; i++) {
       for(var j = 1; j <= gridSize; j++) {
         var rowIdentifier = "ol#row" + i;
-        $(rowIdentifier).append("<li>" + copiedGrid[0] + "</li>");
+        var tileCoordinate = "row" + i + "col" + j;
+        $(rowIdentifier).append("<li class='" + tileCoordinate + "' onclick='game.methods.flipTile(\"" + tileCoordinate + "\")'><span>" + copiedGrid[0] + "</span></li>");
         copiedGrid.splice(0, 1);
       }
     }
@@ -47,9 +49,63 @@ $(document).ready(function() {
 
   drawGrid(numberOfRows, generatedGrid);
 
+
+  
+
+
+
+
+
+
+
+
 });
 
 
+var game = {
+  methods: {
+    flipTile: function(tileToFlip) {
+      if (game.totalMatches === null) {
+        game.totalMatches = ($("ol#tiles li ol").length * $("ol#tiles li ol").length) / 2;
+      }
+      if (game.totalMatches === 0) {
+        console.log("Refresh the page to start a new game.");
+      }
+      if (game.turn[0] === null) {
+        game.turn[0] = tileToFlip;
+        $("." + tileToFlip).toggleClass("flipped");
+      }
+      else if (game.turn[1] === null) {
+        game.turn[1] = tileToFlip;
+        if ( game.turn[0] !== game.turn[1] ) {
+          $("." + tileToFlip).toggleClass("flipped");
+          setTimeout(game.methods.finishTurn, 500);
+        }
+        else {
+          game.turn[1] = null;
+        }
+      }
+    },
+    finishTurn: function() {
+      if ( $("." + game.turn[0] + " span").text() === $("." + game.turn[1] + " span").text() ) {
+        console.log("A match was found!");
+        game.turn = [null, null];
+        game.totalMatches--;
+        console.log(game.totalMatches);
+        if (game.totalMatches === 0) {
+          window.alert("You win!");
+        }
+      }
+      else {
+        $("." + game.turn[0]).toggleClass("flipped");
+        $("." + game.turn[1]).toggleClass("flipped");
+        game.turn = [null, null];
+      }
+    }
+  },
+  turn: [null, null],
+  totalMatches: null
+};
 
 
 
