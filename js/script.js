b766d5cf7e23b234107faf868983507da329e4e4
Jason <devilshaircut@gmail.com>
2015-03-23 16:07:58 -0400

    Add in documentation for the game logic.

--- js/script.js
+++ js/script.js
@@ -52,8 +52,18 @@ $(document).ready(function() {
 });
 
 
+// This object contains the matching game's control logic.
 var game = {
+  // Each turn consists of 2 tile selections.
+  // We need to store each of these tile selections for the purposes of comparing them and seeing if they are a match.
+  turn: [null, null],
+  // A game board has a set number of matching pairs.
+  // We need to store this number so that when matching pairs are identified, we can count down to 0 remaining matching pairs.
+  // This is the player's win condition.
+  totalMatches: null,
+  // This is an object for storing game methods.
   methods: {
+    // This method is called onclick - when a user clicks a tile.
     flipTile: function(tileToFlip) {
       // If this is the first time the game is run, set the number of tile pairs to find.
       // This number depends on the size of the game board.
@@ -70,22 +80,29 @@ var game = {
       if ( $("." + tileToFlip).hasClass("flipped") ) {
         console.log("This tile is already flipped over! Select another tile.");
       }
+      // Check to see if this is the first tile selection in the turn.
+      // If it is, flip the tile over.
       else if (game.turn[0] === null) {
         game.turn[0] = tileToFlip;
         $("." + tileToFlip).toggleClass("flipped");
       }
+      // Check to see if this is the second tile selection in the turn.
+      // If it is, flip the tile over - then finish the turn.
+      // Finishing the turn will either leave both tiles up (if the tiles match),
+      // or it will revert both tiles to their original state (if the tiles don't match).
       else if (game.turn[1] === null) {
         game.turn[1] = tileToFlip;
-        if ( game.turn[0] !== game.turn[1] ) {
-          $("." + tileToFlip).toggleClass("flipped");
-          setTimeout(game.methods.finishTurn, 500);
-        }
-        else {
-          game.turn[1] = null;
-        }
+        $("." + tileToFlip).toggleClass("flipped");
+        setTimeout(game.methods.finishTurn, 500);
       }
     },
+    // This method is called when two tiles have been flipped,
+    // It decides what action to take based on the two flipped tiles.
     finishTurn: function() {
+      // Check to see if the tiles match.
+      // If they do, reset the turn and note that a match was found.
+      // Also check to see if it was the last match that was found.
+      // If it is the last match that was found, inform the player that they won.
       if ( $("." + game.turn[0] + " span").text() === $("." + game.turn[1] + " span").text() ) {
         game.turn = [null, null];
         game.totalMatches--;
@@ -93,15 +110,15 @@ var game = {
           window.alert("You win!");
         }
       }
+      // The tiles didn't match.
+      // Reset the turn and revert the tiles to their original state.
       else {
         $("." + game.turn[0]).toggleClass("flipped");
         $("." + game.turn[1]).toggleClass("flipped");
         game.turn = [null, null];
       }
     }
-  },
-  turn: [null, null],
-  totalMatches: null
+  }
 };
 
 
