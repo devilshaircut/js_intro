7dce009a6ffbd91b8ea56f539eaf86510dbd116f
Jason <devilshaircut@gmail.com>
2015-03-23 17:19:48 -0400

    Add in full JS documentation.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,70 @@
-
+// This function initializes the game grid, with dimensions defined in the HTML/DOM.
 $(document).ready(function() {
 
+  // Store the number of rows.
+  // We assume that the grid will always be a square, so we don't need to know the number of columns.
   var numberOfRows = $("ol#tiles li ol").length;
 
+  // This is a function that generates the game grid.
+  // The grid will be represented as a single serialized array of tile values.
+  // Tile values will each be repeated 1 time, to represent each individual in a pair.
   var generateGrid = function(gridSize) {
+    // Check to make sure the grid has an even number of tiles.
+    // Since each tile has 1 pair, this is mandatory.
     if (gridSize%2 !== 0) {
       window.alert("Grid must have an even number of tiles!");
     }
+    // If the grid has an even number of tiles, make the grid.
     else {
+      // Calculate the total number of tiles.
       var totalTiles = gridSize * gridSize;
+      // Calculate the maximum value on any given tile.
       var maxTileValue = totalTiles / 2;
+      // This function creates a basic, ordered grid.
       var createGrid = function(highestTileValue) {
-        var emptyGrid = [];
+        var emptyGrid = [];                             // Initialize the basic, ordered grid.
         for(var i = 0; i < highestTileValue; i++) {
-          emptyGrid.push(i);
-          emptyGrid.push(i);
+          emptyGrid.push(i);                            // Begin populating the basic, ordered grid with number values.
+          emptyGrid.push(i);                            // Do this twice, so that each tile has a paired tile.
         }
         return emptyGrid;
       };
+      // This function takes a basic, ordered grid, and randomizes it.
       var randomizeGrid = function(grid) {
-        var newGrid = [];
+        var newGrid = [];                               // Initialize the new grid.
         for(var j = 0; j < grid.length;) {
-          var chosenIndex = Math.floor(Math.random() * grid.length);
-          newGrid.push(grid[chosenIndex]);
-          grid.splice(chosenIndex, 1);
+          var chosenIndex = Math.floor(Math.random() * grid.length);    // Randomly choose an element from the unordered grid.
+          newGrid.push(grid[chosenIndex]);              // Add this element to the new grid.
+          grid.splice(chosenIndex, 1);                  // Remove this element from the old grid.
         }
         return newGrid;
       };
-      var orderedGrid = createGrid(maxTileValue);
-      var finalGrid = randomizeGrid(orderedGrid);
+      // Run the grid construction functions and return the grid.
+      var orderedGrid = createGrid(maxTileValue);       // Create the ordered grid.
+      var finalGrid = randomizeGrid(orderedGrid);       // Take the ordered grid and scramble it.
       return finalGrid;
     }
   };
 
+  // Create the grid.
   var generatedGrid = generateGrid(numberOfRows);
 
+  // We need to take a grid and draw it into the HTML/DOM.
   var drawGrid = function(gridSize, grid) {
-    var copiedGrid = grid;
+    // Iterate through the rows.
+    // Continue until the provided base grid is empty.
     for(var i = 1; i <= gridSize; i++) {
+      var rowIdentifier = "ol#row" + i;
+      // Iterate through the columns in each row.
+      // Each of these is a tile.
       for(var j = 1; j <= gridSize; j++) {
-        var rowIdentifier = "ol#row" + i;
+        // Create a unique ID for the tile.
         var tileCoordinate = "row" + i + "col" + j;
-        $(rowIdentifier).append("<li class='" + tileCoordinate + "' onclick='game.methods.flipTile(\"" + tileCoordinate + "\")'><span>" + copiedGrid[0] + "</span></li>");
-        copiedGrid.splice(0, 1);
+        // Create the tile by appending the tile's HTML to its parent row.
+        // Also assign its unique ID and onclick event (thus tying it to the game control logic).
+        $(rowIdentifier).append("<li class='" + tileCoordinate + "' onclick='game.methods.flipTile(\"" + tileCoordinate + "\")'><span>" + grid[0] + "</span></li>");
+        // Remove the appended tile from the base grid.
+        grid.splice(0, 1);
       }
     }
   };
@@ -68,7 +90,7 @@ var game = {
       // If this is the first time the game is run, set the number of tile pairs to find.
       // This number depends on the size of the game board.
       if (game.totalMatches === null) {
-        game.totalMatches = ($("ol#tiles li ol").length * $("ol#tiles li ol").length) / 2;
+        game.totalMatches = ($("ol#tiles li ol").length * $("ol#tiles li ol").length) / 2;    // Use jQuery to determine the number of rows. The grid is always a square, so the number of rows squared, divided by 2 represents the number of pairs.
       }
       // Every time a match is found, the counter decrements by 1.
       // If the counter is 0, the game already ended so no gameplay should take place.
@@ -83,17 +105,17 @@ var game = {
       // Check to see if this is the first tile selection in the turn.
       // If it is, flip the tile over.
       else if (game.turn[0] === null) {
-        game.turn[0] = tileToFlip;
-        $("." + tileToFlip).toggleClass("flipped");
+        game.turn[0] = tileToFlip;                      // Set the first tile.
+        $("." + tileToFlip).toggleClass("flipped");     // Flip the first tile.
       }
       // Check to see if this is the second tile selection in the turn.
       // If it is, flip the tile over - then finish the turn.
       // Finishing the turn will either leave both tiles up (if the tiles match),
       // or it will revert both tiles to their original state (if the tiles don't match).
       else if (game.turn[1] === null) {
-        game.turn[1] = tileToFlip;
-        $("." + tileToFlip).toggleClass("flipped");
-        setTimeout(game.methods.finishTurn, 500);
+        game.turn[1] = tileToFlip;                      // Set the second tile.
+        $("." + tileToFlip).toggleClass("flipped");     // Flip the second tile.
+        setTimeout(game.methods.finishTurn, 500);       // In 0.5 seconds, resolve the turn.
       }
     },
     // This method is called when two tiles have been flipped,
@@ -104,18 +126,18 @@ var game = {
       // Also check to see if it was the last match that was found.
       // If it is the last match that was found, inform the player that they won.
       if ( $("." + game.turn[0] + " span").text() === $("." + game.turn[1] + " span").text() ) {
-        game.turn = [null, null];
-        game.totalMatches--;
-        if (game.totalMatches === 0) {
+        game.turn = [null, null];                       // Reset the turn array to its zero'ed out state.
+        game.totalMatches--;                            // Decrement the game counter.
+        if (game.totalMatches === 0) {                  // If the game counter is 0, the game is over.
           window.alert("You win!");
         }
       }
       // The tiles didn't match.
       // Reset the turn and revert the tiles to their original state.
       else {
-        $("." + game.turn[0]).toggleClass("flipped");
-        $("." + game.turn[1]).toggleClass("flipped");
-        game.turn = [null, null];
+        $("." + game.turn[0]).toggleClass("flipped");   // Flip the cards back to their hidden state.
+        $("." + game.turn[1]).toggleClass("flipped");   // Flip the cards back to their hidden state.
+        game.turn = [null, null];                       // Reset the turn array to its zero'ed out state.
       }
     }
   }
